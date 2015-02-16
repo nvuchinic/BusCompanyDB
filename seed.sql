ef2c77449899ebc6377f7513116b412a610c91a3
nvuchinic <nermin.vucinic@gmail.com>
2015-02-16 09:23:22 +0100

    Create seed.sql

---
+++ seed.sql
@@ -0,0 +1,46 @@
+insert into Vozaci values(1,"boro","drljaca",44);
+insert into Vozaci values(?,"saban","saulic",45);
+insert into Vozaci values(?,"miroslav","ilic",55);
+insert into Vozaci values(1,"boro","drljaca",44);
+insert into Vozaci values(1,"sinan","sakic",44);
+
+insert into Vozila values(1,1);
+insert into Vozila values(?,1);
+insert into Vozila values(?,1);
+insert into Vozila values(?,2;
+insert into Vozila values(?,2);
+insert into Vozila values(?,2);
+insert into Vozila values(?,3);
+insert into Vozila values(?,3);
+insert into Vozila values(?,3);
+
+insert into Tip_vozila(1,"autobus");
+insert into Tip_vozila(?,"trolejbus");
+insert into Tip_vozila(?,"tramvaj");
+
+
+
+insert into Smjene values(1,I);
+insert into Smjene values(?,II);
+insert into Smjene values(?,III);
+
+insert into Linije values(1,"Dobrinja","Trg Austrije",60,"6.00");
+insert into Linije values(?,"Dobrinja","Otoka",30,"6.00");
+insert into Linije values(?,"Dobrinja","Trg Austrije",60,"6.00");
+
+insert into vozaci_vozila(1,1);
+insert into Tip_vozila(1,2);
+insert into Tip_vozila(2,3);
+insert into Tip_vozila(2,4);
+insert into Tip_vozila(3,5);
+insert into Tip_vozila(3,6);
+insert into Tip_vozila(4,7);
+insert into Tip_vozila(4,8);
+insert into Tip_vozila(5,5);
+insert into Tip_vozila(5,9);
+
+
+
+
+
+
