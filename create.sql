78d6fa40cd2d9ee22c85e2fa35f81a464f1dcc21
nvuchinic <nermin.vucinic@gmail.com>
2015-02-19 16:59:26 +0100

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,14 +1,11 @@
 
 
 create table Vozila(
-	ID_vozila integer primary key,
-	tip_id integer,
-	foreign key(tip_id) REFERENCES Tip_vozila
+
+	vozilo_id integer primary key,
+	tip varchar not null,
 );
 
-create table Tip_vozila(
-tip_ID integer primary key,
-	tip_vozila varchar (15));
 
 create table Vozaci(
 ID_vozaca integer primary key,
@@ -18,28 +15,23 @@ starost integer
 );
 
 create table  Linije(
-	redni_broj integer primary key,
+	br_linije integer primary key,
 	prva_stanica varchar(20),
 	zadnja_stanica varchar(20),
 	interval integer,
 	pocetak varchar (5),
-	id_vozila integer,
+	
 	
 );
 
+
 create table Smjene(
-	ID_smjene integer primary key autoincrement,
- vrsta varchar(5)
- );
-
-create table vozila_vozaci(
-vozac_id integer,
-vozilo_id integer,
-foreign key(vozac_id) references Vozaci on update cascade on delete cascade,
-foreign key (vozilo_id) references Vozila on update cascade on delete cascade
-);
-
-create table vozaci_linije(
-vozac_id integer,
-linija_id integer
+	id_vozaca integer, 
+	id_vozila integer,
+	id_linije integer,
+	broj integer default 1,
+	id integer primary key autoincrement,
+	foreign key (id_vozaca) references Vozaci on update cascade on delete cascade,
+	foreign key (id_vozila) references Vozila on update cascade on delete cascade,	
+	foreign key (id_linije) references Linije on update cascade on delete cascade
 );
