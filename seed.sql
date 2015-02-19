42a4d3dac16ebeb68eac18a2e5bff82c4a309ad8
nvuchinic <nermin.vucinic@gmail.com>
2015-02-19 16:58:34 +0100

    Update seed.sql

--- seed.sql
+++ seed.sql
@@ -4,20 +4,34 @@ insert into Vozaci values(3,"miroslav","ilic",55);
 insert into Vozaci values(4,"marinko","rokvic",44);
 insert into Vozaci values(5,"sinan","sakic",44);
 
-insert into Vozila values(1,1);
-insert into Vozila values(2,1);
-insert into Vozila values(3,2);
-insert into Vozila values(4,2;
-insert into Vozila values(5,3);
-insert into Vozila values(6,3);
+insert into Vozila values(1,"autobus");
+insert into Vozila values(2,"autobus");
+insert into Vozila values(3,"trolejbus");
+insert into Vozila values(4,"trolejbus";
+insert into Vozila values(5,"tramvaj");
+insert into Vozila values(6,"tramvaj");
 
-insert into Tip_vozila values(1,"autobus");
-insert into Tip_vozila values(2,"trolejbus");
-insert into Tip_vozila values(3,"tramvaj");
-
-insert into Smjene values(1,I);
-insert into Smjene values(2,II);
-insert into Smjene values(3,III);
+insert into Smjene values(1,7, 4, 1, 1);
+insert INTO Smjene values(2,7, 2, 1, 1);
+insert INTO Smjene values(3,1, 8, 2, 1,);
+insert INTO Smjene values(4,1, 10, 2, 1);
+insert INTO Smjene values(5,3, 10, 4, 1);
+insert INTO Smjene values(6,3, 9, 4, 1);
+insert INTO Smjene values(7,2, 2, 3, 1);
+insert INTO Smjene values(8,2, 3, 3, 1);
+insert INTO Smjene values(9,5, 7, 6, 2);
+insert INTO Smjene values(10,5, 5, 6, 2);
+insert INTO Smjene values(11,4, 1, 8, 2);
+insert INTO Smjene values(12,4, 3, 8, 2);
+insert INTO Smjene values(13,10, 3, 5, 2);
+insert INTO Smjene values(14,10, 2, 5, 2);
+insert INTO Smjene values(15,6, 9, 10, 3);
+insert INTO Smjene values(16,6, 10, 10, 3);
+insert INTO Smjene values(17,8, 5, 9, 3);
+insert INTO Smjene values(18,8, 6, 9, 3);
+insert INTO Smjene values(19,9, 6, 7, 3);
+insert INTO Smjene values(20,9, 7, 7, 3);
+insert INTO Smjene values(21,7, 2, 10, 3);
 
 insert into Linije values(1,"Dobrinja","Trg Austrije",60,"6.00");
 insert into Linije values(2,"Dobrinja","Otoka",30,"6.00");
@@ -26,26 +40,6 @@ insert into Linije values(4,"Dobrinja","Vijecnica",20,"6.00");
 insert into Linije values(5,"Dobrinja","Jezero",60,"6.00");
 insert into Linije values(6,"Ilidza","Bascarsija",20,"6.00");
 
-insert into vozaci_vozila values(1,1);
-insert into vozaci_vozila values(1,2);
-insert into vozaci_vozila values(2,2);
-insert into vozaci_vozila values(2,3);
-insert into vozaci_vozila values(3,1);
-insert into vozaci_vozila values(3,3);
-insert into vozaci_vozila values(4,4);
-insert into vozaci_vozila values(4,5);
-insert into vozaci_vozila values(4,6);
-
-insert into vozaci_linije values(1,1);
-insert into vozaci_linije values(1,5);
-insert into vozaci_linije values(2,2);
-insert into vozaci_linije values(2,4);
-insert into vozaci_linije values(3,6);
-insert into vozaci_linije values(3,3);
-insert into vozaci_linije values(4,1);
-insert into vozaci_linije values(4,5);
-insert into vozaci_linije values(5,3);
-insert into vozaci_linije values(5,6);
 
 
 
