63967339db3f6db871f0ffebb2273fee4462a9e4
nvuchinic <nermin.vucinic@gmail.com>
2015-02-19 17:29:18 +0100

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,5 +1,30 @@
 /*izlistava vozace i vozila koja su koristitili*/
 select Vozaci.ime as ime, Vozaci.prezime as prezime,
-Vozaci.ID_vozaca as id, Tip_vozila.tip as tip_vozila from Vozaci
-inner join vozila_vozaci on Vozaci.ID_vozaca = vozila_vozaci.id_vozaca
-inner join vozila_vozaci on vozila_vozaci.id_vozila = Vozila.ID_vozila;
+	Vozaci.id as id, Vozila.tip as vozilo from Vozaci
+	inner join Smjene on Vozaci.id = Smjene.id_vozaca
+	inner join Vozila on Smjene.id_vozila = Vozila.id
+;
+
+/*izlistava tramvajske linije*/
+select Linije.prva_stanica as pocetna_stanica,
+	Linije.zadnja_stanica as krajnja_stanica from Linije
+	inner join Smjene on Linije.id = Smjene.id_linije where Smjene.id_vozila=5 || Smjene.id_vozila=6;
+;
+
+/*izlistava trolejbuske linije*/
+select Linije.prva_stanica as pocetak,
+	Linije.zadnja_stanica as krajnja_stanica from Linije
+	inner join Smjene on Linije.id = Smjene.id_linije 
+	where Smjene.id_vozila=3 || Smjene.id_vozila=4;
+;
+
+/*izlistava autobuske linije*/
+select Linije.prva_stanica as pocetak,
+	Linije.zadnja_stanica as kraj from Linije
+	inner join Smjene on Linije.id = Smjene.id_linije where Smjene.id_vozila=1 || Smjene.id_vozila=2;
+;
+
+/*izlistava autobuske i trolejbuske stanice*/
+select Linije.prva_stanica as stanica from Linije
+	inner join Smjene on Linije.id = Smjene.id_linije
+	where Smjene.id_vozila < 5;
