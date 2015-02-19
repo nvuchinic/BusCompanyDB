418e1431742caad72b26e3c521602c4d367fd710
nvuchinic <nermin.vucinic@gmail.com>
2015-02-19 08:03:00 +0100

    Update seed.sql

--- seed.sql
+++ seed.sql
@@ -1,43 +1,43 @@
-insert into Vozaci values(?,"boro","drljaca",44);
-insert into Vozaci values(?,"saban","saulic",45);
-insert into Vozaci values(?,"miroslav","ilic",55);
-insert into Vozaci values(?,"marinko","rokvic",44);
-insert into Vozaci values(?,"sinan","sakic",44);
+insert into Vozaci values(1,"boro","drljaca",44);
+insert into Vozaci values(2,"saban","saulic",45);
+insert into Vozaci values(3,"miroslav","ilic",55);
+insert into Vozaci values(4,"marinko","rokvic",44);
+insert into Vozaci values(5,"sinan","sakic",44);
 
 insert into Vozila values(1,1);
-insert into Vozila values(?,1);
-insert into Vozila values(?,1);
-insert into Vozila values(?,2;
-insert into Vozila values(?,2);
-insert into Vozila values(?,2);
-insert into Vozila values(?,3);
-insert into Vozila values(?,3);
-insert into Vozila values(?,3);
+insert into Vozila values(2,1);
+insert into Vozila values(3,2);
+insert into Vozila values(4,2;
+insert into Vozila values(5,3);
+insert into Vozila values(6,3);
+
 
 insert into Tip_vozila(1,"autobus");
-insert into Tip_vozila(?,"trolejbus");
-insert into Tip_vozila(?,"tramvaj");
+insert into Tip_vozila(2,"trolejbus");
+insert into Tip_vozila(3,"tramvaj");
 
 
 
 insert into Smjene values(1,I);
-insert into Smjene values(?,II);
-insert into Smjene values(?,III);
+insert into Smjene values(2,II);
+insert into Smjene values(3,III);
 
 insert into Linije values(1,"Dobrinja","Trg Austrije",60,"6.00");
 insert into Linije values(?,"Dobrinja","Otoka",30,"6.00");
 insert into Linije values(?,"Dobrinja","Trg Austrije",60,"6.00");
 
 insert into vozaci_vozila(1,1);
-insert into Tip_vozila(1,2);
-insert into Tip_vozila(2,3);
-insert into Tip_vozila(2,4);
-insert into Tip_vozila(3,5);
-insert into Tip_vozila(3,6);
-insert into Tip_vozila(4,7);
-insert into Tip_vozila(4,8);
-insert into Tip_vozila(5,5);
-insert into Tip_vozila(5,9);
+insert into vozaci_vozila(1,2);
+insert into vozaci_vozila(2,2);
+insert into vozaci_vozila(2,3);
+insert into vozaci_vozila(3,1);
+insert into vozaci_vozila(3,3);
+insert into vozaci_vozila(4,4);
+insert into vozaci_vozila(4,5);
+insert into vozaci_vozila(4,6);
+
+
+
 
 
 
